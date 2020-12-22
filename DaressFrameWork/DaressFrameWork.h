08a63948fe54f0a0d3fe22834d239571b91b89d7
Ahmed Mahmoud <amabdallah@tendegrees.sa>
2020-12-22 17:55:09 +0200

    first commit

---
+++ DaressFrameWork/DaressFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  DaressFrameWork.h
+//  DaressFrameWork
+//
+//  Created by Shimaa on 12/22/20.
+//  Copyright © 2020 TenDegrees. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DaressFrameWork.
+FOUNDATION_EXPORT double DaressFrameWorkVersionNumber;
+
+//! Project version string for DaressFrameWork.
+FOUNDATION_EXPORT const unsigned char DaressFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DaressFrameWork/PublicHeader.h>
+
+
